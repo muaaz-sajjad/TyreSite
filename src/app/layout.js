1afdcfa43b9457fb1595ae55afb3aa1538f1f8d7
muaaz-sajjad <muaazsajjad777@gmail.com>
2026-04-09 23:56:57 +0500

    Added Google Tag Manager to layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
-'use client'
 import { Outfit, Inter } from "next/font/google";
 import "./globals.css";
 import StructuredData from "@/components/StructuredData";
-import Script from "next/script";
+import GTMScript from "@/components/GTMScript"; // import client component
 
 const outfit = Outfit({
   variable: "--font-outfit",
@@ -53,28 +52,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-	<Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;
-          f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-5DZRTW6Z');`,
-        }}
-      />
       <body className={`${outfit.variable} ${inter.variable}`}>
-	{/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5DZRTW6Z"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
+	<GTMScript />
         <StructuredData />
         {children}
       </body>
